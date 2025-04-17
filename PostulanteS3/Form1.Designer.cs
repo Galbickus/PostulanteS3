ec7849d5712d5bb2039fe40f61eee6557642a19e
Veronica <vgalbicka@gmail.com>
2025-04-17 18:41:39 -0300

    init

--- PostulanteS3/Form1.Designer.cs
+++ PostulanteS3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PostulanteS3
 {
-    partial class Form1
+    partial class FrmPrincipal
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblNombre = new Label();
+            lblApellido = new Label();
+            lblDocumento = new Label();
+            lblTipo = new Label();
+            btnIngresar = new Button();
+            btnLimpiar = new Button();
+            cboTipo = new ComboBox();
+            txtNombre = new TextBox();
+            txtApellido = new TextBox();
+            txtDocumento = new TextBox();
+            SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(57, 54);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(64, 20);
+            lblNombre.TabIndex = 0;
+            lblNombre.Text = "Nombre";
+            // 
+            // lblApellido
+            // 
+            lblApellido.AutoSize = true;
+            lblApellido.Location = new Point(57, 120);
+            lblApellido.Name = "lblApellido";
+            lblApellido.Size = new Size(66, 20);
+            lblApellido.TabIndex = 1;
+            lblApellido.Text = "Apellido";
+            // 
+            // lblDocumento
+            // 
+            lblDocumento.AutoSize = true;
+            lblDocumento.Location = new Point(392, 183);
+            lblDocumento.Name = "lblDocumento";
+            lblDocumento.Size = new Size(87, 20);
+            lblDocumento.TabIndex = 2;
+            lblDocumento.Text = "Documento";
+            // 
+            // lblTipo
+            // 
+            lblTipo.AutoSize = true;
+            lblTipo.Location = new Point(57, 183);
+            lblTipo.Name = "lblTipo";
+            lblTipo.Size = new Size(39, 20);
+            lblTipo.TabIndex = 3;
+            lblTipo.Text = "Tipo";
+            // 
+            // btnIngresar
+            // 
+            btnIngresar.Location = new Point(517, 45);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(94, 29);
+            btnIngresar.TabIndex = 4;
+            btnIngresar.Text = "Ingresar";
+            btnIngresar.UseVisualStyleBackColor = true;
+            btnIngresar.Click += btnIngresar_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new Point(517, 111);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(94, 29);
+            btnLimpiar.TabIndex = 5;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // cboTipo
+            // 
+            cboTipo.AutoCompleteCustomSource.AddRange(new string[] { "DNI", "Pasaporte", "Extranjero" });
+            cboTipo.FormattingEnabled = true;
+            cboTipo.Items.AddRange(new object[] { "DNI", "Pasaporte", "Extranjero" });
+            cboTipo.Location = new Point(161, 175);
+            cboTipo.Name = "cboTipo";
+            cboTipo.Size = new Size(204, 28);
+            cboTipo.TabIndex = 6;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(161, 51);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(204, 27);
+            txtNombre.TabIndex = 7;
+            // 
+            // txtApellido
+            // 
+            txtApellido.Location = new Point(161, 111);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(204, 27);
+            txtApellido.TabIndex = 8;
+            // 
+            // txtDocumento
+            // 
+            txtDocumento.Location = new Point(517, 180);
+            txtDocumento.Name = "txtDocumento";
+            txtDocumento.Size = new Size(179, 27);
+            txtDocumento.TabIndex = 9;
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 255, 192);
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtDocumento);
+            Controls.Add(txtApellido);
+            Controls.Add(txtNombre);
+            Controls.Add(cboTipo);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnIngresar);
+            Controls.Add(lblTipo);
+            Controls.Add(lblDocumento);
+            Controls.Add(lblApellido);
+            Controls.Add(lblNombre);
+            Name = "FrmPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Postulante";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblNombre;
+        private Label lblApellido;
+        private Label lblDocumento;
+        private Label lblTipo;
+        private Button btnIngresar;
+        private Button btnLimpiar;
+        private ComboBox cboTipo;
+        private TextBox txtNombre;
+        private TextBox txtApellido;
+        private TextBox txtDocumento;
     }
 }
--- PostulanteS3/Form1.cs
+++ PostulanteS3/Form1.cs
@@ -1,10 +1,27 @@
 namespace PostulanteS3
 {
-    public partial class Form1 : Form
+    public partial class FrmPrincipal : Form
     {
-        public Form1()
+        public FrmPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {   // Obtener los valores de los TextBox
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            string documento = txtDocumento.Text;
+
+            // Obtener el valor seleccionado del ComboBox
+            string tipoSeleccionado = cboTipo.SelectedItem?.ToString() ?? "No seleccionado";
+
+            // Crear el mensaje para mostrar en el cuadro de diálogo
+            string mensaje = $"Nombre: {nombre}\nApellido: {apellido}\nDocumento: {documento}\nTipo: {tipoSeleccionado}";
+
+            // Mostrar el mensaje en un cuadro de diálogo
+            MessageBox.Show(mensaje, "Datos Ingresados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
     }
 }
